8e66a952643718ffd52c874175208d65bb38cca9
Medhad <medhad.ai@myant.ca>
2025-06-24 10:18:13 -0400

    MYP-3837: iOS build fix due to deprecated version.

--- libphonenumber_plugin/ios/Classes/SwiftLibphonenumberPlugin.swift
+++ libphonenumber_plugin/ios/Classes/SwiftLibphonenumberPlugin.swift
@@ -4,7 +4,7 @@ import PhoneNumberKit
 
 public class SwiftLibphonenumberPlugin: NSObject, FlutterPlugin {
     
-    let phoneNumberKit = PhoneNumberUtility()
+    let phoneNumberKit = PhoneNumberKit()
     
     public static func register(with registrar: FlutterPluginRegistrar) {
         let channel = FlutterMethodChannel(name: "plugin.libphonenumber", binaryMessenger: registrar.messenger())
