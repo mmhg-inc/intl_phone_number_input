471cc229c3de875badaddd4b75a768955d50ad71
Medhad <medhad.ai@myant.ca>
2025-06-25 12:43:55 -0400

    MYP-3837: Revert back to PhoneNumberUtility API

--- libphonenumber_plugin/ios/Classes/SwiftLibphonenumberPlugin.swift
+++ libphonenumber_plugin/ios/Classes/SwiftLibphonenumberPlugin.swift
@@ -4,7 +4,7 @@ import PhoneNumberKit
 
 public class SwiftLibphonenumberPlugin: NSObject, FlutterPlugin {
     
-    let phoneNumberKit = PhoneNumberKit()
+    let phoneNumberKit = PhoneNumberUtility()
     
     public static func register(with registrar: FlutterPluginRegistrar) {
         let channel = FlutterMethodChannel(name: "plugin.libphonenumber", binaryMessenger: registrar.messenger())
